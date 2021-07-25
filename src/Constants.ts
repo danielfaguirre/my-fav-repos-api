1281543c4f857646dcc62a1380276dd3976fdc4e
Daniel <danielfernaco@gmail.com>
2021-07-25 15:04:26 -0500

    Change access token

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,5 @@
-export const GITHUB_ACCESS_TOKEN = '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';
+//export const GITHUB_ACCESS_TOKEN = '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';
+export const GITHUB_ACCESS_TOKEN = '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';
 export const GITHUB_END_POINT = 'https://api.github.com/graphql';
 export const BD_DIRECTORY = './src/bd';
 export const SERVER_PORT = 3001;
--- src/Provider/GHRepository.provider.ts
+++ src/Provider/GHRepository.provider.ts
@@ -25,7 +25,6 @@ export default class GHRepositoryProvider implements IGHRepository {
             const body = await response.json();
             return transformGHData(body, ghRepositories, userName);
         } catch (error) {
-            console.log(error);
             return [];
         }
     }
